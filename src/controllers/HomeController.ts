26dfc901007b32daf0428890fe613a2a910c70bd
Stanislas <stanislasakakpo00@gmail.com>
2024-11-11 22:53:40 +0100

    Ajout du html dans le fichier

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,11 +1,36 @@
 
 import { Context } from 'hono'
-
-
+import { html } from 'hono/html';
 
 class HomeController{
     public static getHome(c:Context){
-        return c.text ("Bienvenue dans l'application de gestion de parkings !");
+        return c.html(html`<!DOCTYPE html>
+
+            <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Welcome to EuroPark</title>
+          </head>
+        <body>
+            <h1>Welcome to EuroPark</h1>
+            <img src="/static/parking.png" alt="EuroPark Parking" />
+            <p>
+              Save time and money with EuroPark! Enjoy a 100% contactless parking experience for a short
+              or long duration in our car parks in Europe!
+            </p>
+            <nav>
+            <ul>
+              <li><a href="/cities">Our Cities</a></li>
+              <li><a href="/parkings">Our Car Parks</a><li>
+              </ul>
+            </nav>
+        </body>
+            
+            
+            
+            `)
+        
     }
 }
 
