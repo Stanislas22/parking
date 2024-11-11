4e7c38bc7876069ffb54a48654e4a1e6ff1466d9
Stanislas <stanislasakakpo00@gmail.com>
2024-11-12 00:36:35 +0100

    Création de ReadAllCitiesController

---
+++ src/controllers/ReadAllCitiesController.ts
@@ -0,0 +1,10 @@
+import { Context } from "hono";
+import { cities } from "../data/staticDatabase";
+import ReadAllCitiesView from "../views/city/ReadAllCitiesView";
+const ReadAllCitiesController = (c:Context)=>{
+    const html1 = ReadAllCitiesView(cities);
+
+    return c.html(html1);
+
+};
+export default ReadAllCitiesController;
