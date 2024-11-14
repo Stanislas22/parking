7aeca79e67977bdee1cb95612402c9f5049f9e9d
Stanislas <stanislasakakpo00@gmail.com>
2024-11-14 01:15:17 +0100

    Modification du code suite à une petite erreur

--- src/controllers/ReadAllCitiesController.ts
+++ src/controllers/ReadAllCitiesController.ts
@@ -1,10 +1,12 @@
 import { Context } from "hono";
-import { cities } from "../data/staticDatabase";
-import {ReadAllCitiesView} from "../views/city/ReadAllCitiesView";
+import { cities, parkings } from "../data/staticDatabase";
+import ReadAllCitiesView from "../views/city/ReadAllCitiesView";
 const ReadAllCitiesController = (c:Context)=>{
-    const html1 = ReadAllCitiesView(cities);
+    const citiesparkings = cities.filter(city=>city.parkingsIds.length>0);
+    const html1 = ReadAllCitiesView({cities:cities,parkings:citiesparkings});
 
     return c.html(html1);
 
 };
+console.log("ville: " , cities)
 export default ReadAllCitiesController;
